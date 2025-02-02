89511d896ad16d5ef336792d986efc18b0fae621
Neal Shah <nealshah04@gmail.com>
2025-02-02 03:34:12 -0500

    removing eslint

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,13 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+// next.config.js
+module.exports = {
+  eslint: {
+    // WARNING: This allows production builds to successfully complete even if your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
+
+
 export default nextConfig;
